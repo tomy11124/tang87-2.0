76c6c3ba00426cbe8c2cdcf2109a86d54edfb456
tomy11124 <tomy11124@gamil.com>
2017-11-10 14:45:54 +0800

    11102

--- httpd/index.js
+++ httpd/index.js
@@ -1,80 +1,80 @@
-'use strict';
+ 'use strict';
 
-let http = require('http');
+ let http = require('http');
 
-http.createServer((request, response) => {
-   let fs = require('fs');
-    let postData = ' ';
-    
-    
-    
-    request.on('data',(chunk)=> {
-    postData += chunk;
+ const routingTable = {
+ '/': {
+ url: '../htdocs/index.html',
+ type: 'text/html'
+ },
+ '/styles.css': {
+ url: '../htdocs/assets/css/styles.css',
+ type: 'text/css'
+ },
+ '/SokobanClone_byVellidragon.png': {
+ url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
+ type: 'text/css'
+ },
+ };
 
-console.log(
-        '接收的POST data片段 k:[' + chunk+ '].'
-         );
-    });
-    
-       request.on{'end',()=>{
-        switch (request.url){
-    case'/':
-                fs.readFile('../htdocs/index.html',(err,data)=>{
-           if(err){
-           console.log('檔案讀取錯誤');
-           }
-                else{    
-    response.writeHead(200, {
-        'Content-Type': 'texe/html'
-});
-                    
-response.write(data);
-                    response.end();
-                }
-                });
-                
-                break;
-                
-            case '/assets/css/styles.css':
-                fs.readFile('../htdocs/assets/css/styles.css',(err,data)=>{
-                if (err){
-                    console.log('檔案讀取錯誤');
-                }
-                    else{
-                        response.writeHead(200,{
-                            'Content-Type':'text/css'
-                        });
-                        
-                        response.write(data);
-                        response.end();
-                    }
-                });
-                
-                break;
-                    
-            case '/assets/png/SokobanClone_byVellidragon.png':
-                       fs.readFile(    
-                       '../htdocs/assets/png/SokobanClone_byVellidragon.png',
-                           (err,data)=>{
-                               if(err){
-                                   console.log('檔案讀取錯誤');
-                               }
-                               else{
-                                   response.write(data);
-                                   response.end();
-                               }
-                           }
-                           );
-                
-                break;
-                
-                default;
-                console.log('為定義的存取:' + request.url);
-                
-                response.end();
-                
-                break;
-                           }
-// log message to Console
-console.log('Server running at http://127.0.0.1:8088/');
-// index.js
+ /**
+
+
+
+
+
+
+
+
+
+ let serve = (response, fname, datatype) => {
+ let fs = require('fs');
+
+ fs.readFile(fname, (err, data) => {
+ if (err) {
+ console.log(' 檔案讀取錯誤');
+ }
+ else {
+ response.writeHead(200, {
+'Content-Type': datatype
+ });
+
+ response.write(data);
+ response.end();
+ }
+ });
+ };
+
+ http.createServer((request, response) => {
+ let fs = require('fs');
+
+ let postData = '';
+
+
+
+ request.on('data', (chunk) => {
+ postData += chunk;
+
+ console.log(
+ ' 接收的 POST data ⽚段 k: [' + chunk + '].'
+ );
+ });
+
+ request.on('end', () => {
+ if (request.url in routingTable) {
+ let obj = routingTable[request.url];
+
+ serve(response, obj.url, obj.type);
+ }
+ else {
+ console.log(' 未定義的存取: ' + request.url);
+
+ response.end();
+ }
+ });
+ }).listen(8088);
+
+ 
+ console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+
+ // index.js
