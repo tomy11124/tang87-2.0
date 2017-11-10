561bda4964f84bf2425914d02639fb8d930cdb5d
tomy11124 <tomy11124@gamil.com>
2017-11-10 15:12:12 +0800

    111004
    This reverts commit 76c6c3ba00426cbe8c2cdcf2109a86d54edfb456.

--- httpd/index.js
+++ httpd/index.js
@@ -1,23 +1,21 @@
 'use strict';
 
-let http = require('http');
+ let http = require('http');
 
-const routingTable = {
-'/': {
-url: '../htdocs/index.html',
-type: 'text/html'
-},
-'/styles.css': {
-url: '../htdocs/assets/css/styles.css',
-type: 'text/css'
-},
-'/SokobanClone_byVellidragon.png': {
-url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
-type: 'text/css'
-},
-};
-
-
+ const routingTable = {
+ '/': {
+ url: '../htdocs/index.html',
+ type: 'text/html'
+ },
+ '/styles.css': {
+ url: '../htdocs/assets/css/styles.css',
+ type: 'text/css'
+ },
+ '/SokobanClone_byVellidragon.png': {
+ url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
+ type: 'text/css'
+ },
+ };
 
 
 
@@ -27,54 +25,56 @@ type: 'text/css'
 
 
 
-let serve = (response, fname, datatype) => {
-let fs = require('fs');
-
-fs.readFile(fname, (err, data) => {
-if (err) {
-console.log(' 檔案讀取錯誤');
-}
-else {
-response.writeHead(200, {
-'Content-Type': datatype
-});
-
-response.write(data);
-response.end();
-}
-});
-};
-
-http.createServer((request, response) => {
-let fs = require('fs');
-
-let postData = '';
 
 
+ let serve = (response, fname, datatype) => {
+ let fs = require('fs');
 
-request.on('data', (chunk) => {
-postData += chunk;
+ fs.readFile(fname, (err, data) => {
+ if (err) {
+ console.log(' 檔案讀取錯誤');
+ }
+ else {
+ response.writeHead(200, {
+ 'Content-Type': datatype
+ });
 
-console.log(
-' 接收的 POST data ⽚段 k: [' + chunk + '].'
-);
-});
+ response.write(data);
+ response.end();
+ }
+ });
+ };
 
-request.on('end', () => {
-if (request.url in routingTable) {
-let obj = routingTable[request.url];
+ http.createServer((request, response) => {
+ let fs = require('fs');
 
-serve(response, obj.url, obj.type);
-}
-else {
-console.log(' 未定義的存取: ' + request.url);
-
-response.end();
-}
-});
-}).listen(8088);
+ let postData = '';
 
 
-console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
 
-// index.js
+ request.on('data', (chunk) => {
+ postData += chunk;
+
+ console.log(
+ ' 接收的 POST data ⽚段 k: [' + chunk + '].'
+ );
+ });
+
+ request.on('end', () => {
+ if (request.url in routingTable) {
+ let obj = routingTable[request.url];
+
+ serve(response, obj.url, obj.type);
+ }
+ else {
+ console.log(' 未定義的存取: ' + request.url);
+
+ response.end();
+ }
+ });
+ }).listen(8088);
+
+ // log message to Console
+ console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+
+ // index.js
